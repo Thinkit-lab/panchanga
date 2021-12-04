910f115bcbfc964a095ac8ac638aa772732d5683
Thinkit-lab <afolabikunle2@gmail.com>
2021-12-04 07:47:40 +0100

    new

--- lib/services/dataBase.dart
+++ lib/services/dataBase.dart
@@ -10,12 +10,12 @@ class DatabaseService {
   // collection reference
   final CollectionReference userCollection = FirebaseFirestore.instance.collection('users');
 
-  Future<void> updateUserData(String name, String gender, String dob, ) async {
+  Future<void> updateUserData(String name, String gender, String dob, String birthTime) async {
     return await userCollection.doc(uid).set({
       'gender': gender,
       'name': name,
       'dob': dob,
-      // 'birthTime': birthTime,
+      'birthTime': birthTime,
       // 'birthPlace': birthPlace,
 
     });
